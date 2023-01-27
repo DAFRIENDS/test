7be12319b880c2477b362620b3817c49004890cc
DAFRIENDS <dadriends1dabest1@gmail.com>
2023-01-27 10:12:18 +0300

    Added new python file

---
+++ KG to LBS converter and vice versa.py
@@ -0,0 +1,14 @@
+pounds = int(input("I am going to convert pounds to kilograms. Just put any amount of pounds once: "))
+kilograms = pounds / 2.20462
+print(f"{kilograms} KG")
+print("Easy. ")
+
+
+kg = int(input("I can also convert kg to lbs. Please insert the amount of kg once: "))
+lbs = kg * 2.20462
+print(f"{lbs} LBS")
+("Also Easy. ")
+
+print('It is very accurate!')
+print('''FUN FACT:
+1KG == 2.20462 LBS/POUNDS''')
